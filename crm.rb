70820c6cd482eb5218a1804d96d10ce67332a322
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-08 15:24:02 -0400

    add ruby code to contacts.erb to display the contacts first and last names

--- crm.rb
+++ crm.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'contact'
+require_relative 'contact'
 
 get '/' do
 	@crm_app_name = "My CRM"
@@ -8,6 +8,11 @@ get '/' do
 end
 
 get '/contacts' do
+	@contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
 	erb :contacts
 end
 
