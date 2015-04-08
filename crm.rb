3430b6fe8dc9e97a657e4d7bbca81084918874d1
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-08 15:42:49 -0400

    created a global variable of an instance of the rolodex class and added it to contacts.erb

--- crm.rb
+++ crm.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
+$rolodex= Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
 	@time = Time.now
@@ -9,16 +11,7 @@ get '/' do
 end
 
 get '/contacts' do
-	@contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
 	erb :contacts
-end
-
-get '/contacts/new' do
-	erb :new
 end
 
 get '/contacts/new' do
