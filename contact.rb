f7feddad2a2aaefcd6cabe151e81f6d9af7195ad
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-08 15:09:40 -0400

    Created new route for /contacts and created a contacts.rb file that contains the Contact Class from the CRM assignment

--- contact.rb
+++ contact.rb
@@ -1,1 +1,19 @@
-contact.rb
+class Contact
+attr_accessor :id, :first_name, :last_name, :email, :notes
+
+	def initialize(first_name, last_name, email, notes)
+		@id = id
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@notes = notes
+	end
+
+	def to_s
+		print "first name: #{@first_name}, "
+		print "last name: #{@last_name}, "
+		print "email: #{@email}, "
+		print "notes: #{@notes}"
+		puts ""
+	end
+end
--- crm.rb
+++ crm.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'contact'
 
 get '/' do
 	@crm_app_name = "My CRM"
 	@time = Time.now
 	erb :index
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/contacts/new' do
+	erb :new
+end
+
+get '/contacts/new' do
+	erb :new
 end
