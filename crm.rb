2cb8323cd13d229e47ece89693299057debf118b
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-10 08:37:27 -0400

    Added a route that that display contact info on individual pages, based on contact ID. Also added CSS styling to the layout and index erbs

--- crm.rb
+++ crm.rb
@@ -14,12 +14,43 @@ get '/contacts' do
 	erb :contacts
 end
 
-get '/contacts/new_contact' do
-	erb :new_contact
-end
-
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
+
+get '/contacts/new_contact' do
+	erb :new_contact
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/resources' do
+	erb :resources
+end
+
+get '/news' do
+	erb :news
+end
+
+get '/contacts/:id' do
+	@contact = $rolodex.find(params[:id].to_i)
+  erb :show
+end
+
+# get '/:menu_page' do
+# 	menu = params[:menu_page]
+# 	erb menu
+# end
+
+# get '/articles/:article_name'
+# 	if File.exist?("views/#{params[:article_name]}.erb")
+# 		erb params[:article_name]
+# 	else
+# 		erb :error_page
+# 	end
+# end
